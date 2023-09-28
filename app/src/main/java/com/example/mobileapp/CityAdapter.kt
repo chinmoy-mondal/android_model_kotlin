5a2c05dd88957f6e6607f9e00ad1d5da776e02f4
Chinmoy Mondal <chinmoymondal791@gmail.com>
2023-09-28 12:15:43 +0600

    listView example

---
+++ app/src/main/java/com/example/mobileapp/CityAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.mobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CityAdapter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/mobileapp/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.mobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cityNames = arrayOf("monisha","sarmistha","chinmoy","chayon","srabonti","sarmistha","chinmoy","chayon","srabonti","sarmistha","chinmoy","chayon","srabonti")
+        val cities:ListView = findViewById(R.id.cities)
+        val cityAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,cityNames)
+
+        cities.adapter = cityAdapter
+
+        cities.setOnItemClickListener { adapterView, view, position, id ->
+            val city:TextView = view as TextView
+            Toast.makeText(this,city.text,Toast.LENGTH_SHORT).show()
+        }
     }
 }
